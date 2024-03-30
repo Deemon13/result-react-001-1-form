104c1cd970ec68b7a6a66f27a29ccd04c49bdc31
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-03-30 11:10:59 +0300

    add styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,44 @@ import styles from './app.module.css';
 export const App = () => {
 	return (
 		<div className={styles.app}>
-			<form>
-				<label>Email:</label>
-				<input type="email" />
-				<label>Пароль:</label>
-				<input type="password" />
-				<label>Повтор пароля:</label>
-				<input type="password" />
-				<button type="submit">Зарегистрироваться</button>
+			<form className={styles.form}>
+				<div className={styles.group}>
+					<label className={styles.label} htmlFor="email">
+						Email:
+					</label>
+					<input
+						className={styles.input}
+						type="email"
+						name="email"
+						id="email"
+					/>
+				</div>
+				<div className={styles.group}>
+					<label className={styles.label} htmlFor="password">
+						Пароль:
+					</label>
+					<input
+						className={styles.input}
+						type="password"
+						name="password"
+						id="password"
+					/>
+				</div>
+				<div className={styles.group}>
+					<label className={styles.label} htmlFor="password-repeat">
+						Повтор пароля:
+					</label>
+					<input
+						className={styles.input}
+						type="password"
+						name="password-repeat"
+						id="password-repeat"
+					/>
+				</div>
+
+				<button className={styles.btn} type="submit">
+					Зарегистрироваться
+				</button>
 			</form>
 		</div>
 	);
