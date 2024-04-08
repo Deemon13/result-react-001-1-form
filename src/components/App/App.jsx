bf32b6b82fd39fb847e0d6df847d7fcf7f9c4bcf
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-04-08 21:50:07 +0300

    remove validation, add basic handlers

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,9 +6,9 @@ const sendFormData = formData => {
 	console.log(formData);
 };
 
-const REG_EMAIL = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
-const REG_PASS =
-	/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/;
+// const REG_EMAIL = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+// const REG_PASS =
+// 	/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/;
 
 export const App = () => {
 	const [data, setData] = useState({
@@ -17,95 +17,10 @@ export const App = () => {
 		checkPassword: '',
 	});
 
-	// const [email, setEmail] = useState('');
-	// const [password, setPassword] = useState('');
-	// const [repPass, setRepPass] = useState('');
-	// const [error, setError] = useState(null);
-
-	const handleChange = ({ target }) => {
-		const { name, value } = target;
-
-		setData(prevState => ({
-			...prevState,
-			[name]: value,
-		}));
-
-		if (!REG_EMAIL.test(value) && name === 'email') {
-			console.log('Invalid email');
-		}
-
-		if (!REG_PASS.test(value) && name === 'password') {
-			console.log('Invalid password');
-		}
-		
-		if (value !== data.password && name === 'checkPassword') {
-			console.log('Passwords not equal');
-		}
-	};
-
 	const handleSubmit = event => {
 		event.preventDefault();
 		sendFormData(data);
-
-		// let newError = null;
-
-		// if (repPass === password) {
-		// 	newError = 'Все Ок!';
-		// } else {
-		// 	newError = 'Пароли не совпадают!';
-		// }
-
-		// setError(newError);
-
-		// console.log(error);
 	};
-
-	// const handlePasswordInput = ({ target }) => {
-	// 	setPassword(target.value);
-
-	// 	let newError = null;
-
-	// 	if (target.value.length > 20) {
-	// 		newError = 'Длина пароля должна быть меньше 20 символов';
-	// 	} else {
-	// 		newError = 'Другие условия!';
-	// 	}
-
-	// 	setError(newError);
-
-	// 	console.log(error);
-	// };
-
-	// const handleRepeatPasswordInput = ({ target }) => {
-	// 	setRepPass(target.value);
-
-	// let newError = null;
-
-	// console.log('repPass:', repPass);
-	// console.log('password:', password);
-
-	// if (repPass === password) {
-	// 	newError = 'Все Ок!';
-	// } else {
-	// 	newError = 'Пароли не совпадают!';
-	// }
-
-	// setError(newError);
-	// };
-
-	// const handleRepeatPasswordChangeFocus = () => {
-	// 	let newError = null;
-
-	// 	if (repPass === password) {
-	// 		newError = 'Все Ок!';
-	// 	} else {
-	// 		newError = 'Пароли не совпадают!';
-	// 	}
-
-	// 	setError(newError);
-
-	// 	console.log(error);
-	// };
 
 	return (
 		<div className={styles.app}>
@@ -117,7 +32,7 @@ export const App = () => {
 					value={data.email}
 					id={'email'}
 					type={'email'}
-					onChange={handleChange}
+					onChange={({ target }) => setData({ ...data, email: target.value })}
 				/>
 				<InputField
 					label={'Пароль:'}
@@ -126,7 +41,9 @@ export const App = () => {
 					value={data.password}
 					id={'password'}
 					type={'password'}
-					onChange={handleChange}
+					onChange={({ target }) =>
+						setData({ ...data, password: target.value })
+					}
 				/>
 				<InputField
 					label={'Повтор пароля:'}
@@ -135,7 +52,9 @@ export const App = () => {
 					value={data.checkPassword}
 					id={'checkPassword'}
 					type={'password'}
-					onChange={handleChange}
+					onChange={({ target }) =>
+						setData({ ...data, checkPassword: target.value })
+					}
 				/>
 				<button className={styles.btn} type="submit">
 					Зарегистрироваться
